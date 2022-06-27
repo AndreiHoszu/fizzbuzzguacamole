310b19aac22d56474ed5d055d875fc38c5427258
Andrei Hoszu <andrei.hoszu@softwire.com>
2022-06-27 14:28:38 +0100

    something

--- fizzbuzz/Program.cs
+++ fizzbuzz/Program.cs
@@ -6,7 +6,58 @@ namespace fizzbuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 255; i++)
+            {
+                String s = "";
+
+                if(i % 3 == 0)
+                {
+                    s = "Fizz";
+                }
+                if(i % 5 == 0)
+                {
+                    s += "Buzz";
+                }
+                if(i % 7 == 0)
+                {
+                    s += "Bang";
+                }
+                if(i % 11 == 0)
+                {
+                    s = "Bong";
+                }
+                if(i % 13 == 0)
+                {
+                    if(s.Contains('B'))
+                    {
+                        s = s.Substring(0, s.IndexOf('B')) + "Fezz" + s.Substring(s.IndexOf('B'));
+                    }
+                    else
+                    {
+                        s += "Fezz";
+                    }
+                }
+                if(i % 17 == 0)
+                {
+                    String x = "";
+
+                    for(int j = s.Length-4; j >= 0; j-=4)
+                    {
+                        x += s.Substring(j, 4);
+                    }
+
+                    s = x;
+                }
+
+                if(s.Length > 0)
+                {
+                    Console.WriteLine(s);
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
